f710c094dd0d481d88727fc67b8fdfc1f51d014c
martin chibwe <mchibwe01@gamil.com>
2015-10-19 02:08:38 -0700

    lab2-working with helloworld file

---
+++ kern/startup/hello.c
@@ -0,0 +1,12 @@
+
+#include <types.h>
+#include <lib.h>
+#include "hello.h"
+
+//#include <test.h>
+
+void hello(void){
+ kprintf("Hello World \n");
+ 
+}
+
---
+++ kern/startup/hello.h
@@ -0,0 +1,8 @@
+
+
+#ifndef hello_h_
+#define hello_h_
+
+void hello(void);
+
+#endif
