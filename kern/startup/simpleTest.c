59e4161f6d99cbc23ef3b74f4398f476330d9a20
martin chibwe <mchibwe01@gamil.com>
2015-11-14 22:15:46 -0800

    added aguments to lab3

--- kern/startup/simpleTest.c
+++ kern/startup/simpleTest.c
@@ -2,6 +2,7 @@
 
 #include <types.h>
 #include <lib.h>
+//#include <stdio.h>
 #include <test.h>
 #include "simpleTest.h"
 
@@ -9,10 +10,22 @@
 
 
 int simpleTest(int narags, char ** args){
-	(void) narags;
-	(void) args;
-
- kprintf("Stand by this is a simple test");
-
- return 0;
+	
+	int i = 0;
+	if(narags == 1)
+	{
+	  kprintf("You only have one  argument. You suck.\n");
+	}else if(narags > 1 && narags < 4){
+		kprintf("Here's your arguments: \n");
+		
+		for(i = 0; i < narags; i++ ){
+		   kprintf("%s", args[i]);
+		}
+	 	kprintf("\n");
+	}else{
+	 kprintf("You have too many arguments. You suck\n");
+	}
+	return 0;
 }
+	
+
